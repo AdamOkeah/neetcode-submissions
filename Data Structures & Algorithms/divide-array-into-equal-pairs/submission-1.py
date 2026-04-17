8b6402dffe3221c4184115ae41d89f3f3850b647
Adam Okeahalam <adamokeahalam@gmail.com>
2026-04-17 23:10:51 +0100

    Add: divide-array-into-equal-pairs - submission-1

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        freq_map = Counter(nums)
+        for x in freq_map.values():
+            if x % 2 != 0:
+                return False
+        
+        return True
