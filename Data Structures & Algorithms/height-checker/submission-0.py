fc6f51349ec5c035f7eee5ec6ef31d89e01dd460
Adam Okeahalam <adamokeahalam@gmail.com>
2026-06-06 18:33:24 +0100

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+
+        output = 0
+
+
+        for x in range(len(heights)):
+            if heights[x] != expected[x]:
+                output +=1 
+
+        return output
+
+        
